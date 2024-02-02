555c27ef832ebd161002db512ec4d2a92eb6efa4
Can Pehlivan <cpehlivan544@gmail.com>
2024-02-02 13:15:18 +0300

    Improved code readability

--- EmployeeModel.cs
+++ EmployeeModel.cs
@@ -7,8 +7,11 @@ namespace LambdaFunctions
     public class EmployeeModel
     {
         public string FirstName { get; set; }
+        
         public string LastName { get; set; }
+        
         public int ID { get; set; }
+        
         public string FullName { get { return $"{FirstName} {LastName}";  } }
     }
 }
